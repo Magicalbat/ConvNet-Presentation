6f6621ec807c90badb0a54c026eb2f0d0dc17291
Magicalbat <magicalbat@comcast.net>
2024-01-02 08:43:19 -0500

    Add turbospork header files

---
+++ turbospork/include/turbospork.h
@@ -0,0 +1,23 @@
+/**
+ * @file turbospork.h
+ * @brief Includes all other turbospork headers
+ */
+
+#ifndef TURBOSPORK_H
+#define TURBOSPORK_H
+
+#include "mg/mg_arena.h"
+
+#include "base_defs.h"
+#include "err.h"
+#include "prng.h"
+#include "str.h"
+#include "os.h"
+#include "tensor.h"
+#include "img.h"
+#include "costs.h"
+#include "optimizers.h"
+#include "layers.h"
+#include "network.h"
+
+#endif // TURBOSPORK_H
